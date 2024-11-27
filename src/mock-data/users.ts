50584d5ce594b4fba9dff368134fd15fc91437c8
diogo-SG <diogo.carneiro@studiographene.com>
2024-11-27 16:16:48 +0000

    added basic user api routes returning json mock data

---
+++ src/mock-data/users.ts
@@ -0,0 +1,22 @@
+export const mockUsers = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@sharklasers.com",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    email: "janedoe@sharklasers.com",
+  },
+  {
+    id: 3,
+    name: "Alice",
+    email: "alice@example.com",
+  },
+  {
+    id: 4,
+    name: "Bob",
+    email: "bob@example.com",
+  },
+];
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,52 @@
 import express from "express";
+import { mockUsers } from "./mock-data/users";
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-/* ------------------------------ Set up routes ----------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                                   Routes                                   */
+/* -------------------------------------------------------------------------- */
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.get("/about", (req, res) => {
-  res.send("About page");
+app.get("/api/users", (req, res) => {
+  // check for a "limit" query parameter
+
+  const queryLimit = req.query.limit;
+
+  if (typeof queryLimit === "undefined") {
+    res.status(200).json(mockUsers);
+    return;
+  }
+
+  const limit = parseInt(queryLimit as string);
+  if (limit < 0 || isNaN(limit)) {
+    res.status(400).send("Limit should be a positive number");
+    return;
+  }
+
+  const slicedUsers = mockUsers.slice(0, limit);
+
+  res.status(200).json(slicedUsers);
 });
+
+app.get("/api/users/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = mockUsers.find((user) => user.id === userId);
+
+  if (!user) {
+    res.status(404).send("User not found");
+    return;
+  }
+
+  res.status(200).json(user);
+});
+
+app.get;
 
 /* ------------------------------ Start server ------------------------------ */
 app.listen(PORT, () => {
